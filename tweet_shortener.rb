07c358f10cfe64113b0c216fab1da5bdecb7601b
Jessica Gladstone <jcg321@comcast.net>
2018-10-06 18:56:37 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+def dictionary
+  sub_hash = {
+    :hello =>'hi',
+    :to => '2',
+    :two =>'2',
+    :too =>'2',
+    :for =>'4',
+    :four =>'4',
+    :be =>'b',
+    :you =>'u',
+    :at=>'@',
+    :and =>'&'
+  }
+  
+end
+def word_substituter(string)
+    tweet = string.split
+    i = 0
+    while i < tweet.length
+      if dictionary.has_key?(tweet[i].downcase)
+        tweet[i] = dictionary[tweet[i].downcase]
+      end
+      i += 1
+    end
+ tweet.join(" ")
+  end
+
+
